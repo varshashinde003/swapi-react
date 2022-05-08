1449d87e0c74b8f673839e2976aa556577747456
varshashinde003 <varshashinde003@gmail.com>
2022-05-08 15:46:34 +0530

    UPDATE API URL

--- src/services/entityservice.js
+++ src/services/entityservice.js
@@ -1,24 +1,22 @@
-const appConstants = require("../appconstants.json")
-export default class EntityService{
-  static async getData(entity,page){
-     try{
-       let data = await fetch(`${appConstants["PROTOCOL"]}://${appConstants["IP_ADDRESS"]}:${appConstants["PORT"]}/starwars/api/${entity}?page=${page}`)
-       data = await data.json(data)
-       return data
-     }catch(e){
-       console.error(e)
-       throw e
-     }
-  }
-  static async getDatum(url){
-    try{
-      let datum = await fetch(`${url}`)
-      datum = await datum.json(datum)
-      return datum
-    }catch(e){
-      console.error(e)
-      throw e
+export default class EntityService {
+  static async getData(entity, page) {
+    try {
+      let data = await fetch(`https://swapi.dev/api/${entity}?page=${page}`);
+      data = await data.json(data);
+      return data;
+    } catch (e) {
+      console.error(e);
+      throw e;
     }
-
+  }
+  static async getDatum(url) {
+    try {
+      let datum = await fetch(`${url}`);
+      datum = await datum.json(datum);
+      return datum;
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
   }
 }
